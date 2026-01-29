572fe1f00b1fe3d8da7c35c3bb86fd7b27398075
siew26 <siewyong0726@gmail.com>
2026-01-29 06:28:53 +0000

    week 1 de zoomcamp complete

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -5,6 +5,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 #instead of waiting the data to process, we can see the progress
 from tqdm.auto import tqdm
+import click
 
 
 
@@ -32,20 +33,19 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-def run():
-    pg_user ='root'
-    pg_pass ='root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--year', default='2021', type=int, help='Year of the data')
+@click.option('--month', default='1', type=int, help='Month of the data')
+@click.option('--chunksize', default='100000', type=int, help='Chunk size for data')
 
-    year=2021
-    month=1
-
-    target_table='yellow_taxi_data'
-
-    chunksize=100000
-
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table, year, month, chunksize):
+    # Ingestion logic here
     prefix='https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
     url=f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
 
